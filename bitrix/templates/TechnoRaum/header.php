1aecf78a0ddace8512677527cbbbdcd754f46462
neeil1990 <neeil@mail.ru>
2019-11-28 11:05:20 +0300

    delete roistat

--- bitrix/templates/TechnoRaum/header.php
+++ bitrix/templates/TechnoRaum/header.php
@@ -54,16 +54,7 @@ if($url[1] != "personal" && $url[2] != "order" && $url[3] != "make")
 		<script>
 			var arrProducts = new Array();
 		</script>
-
-<script>
-(function(w, d, s, h, id) {
-    w.roistatProjectId = id; w.roistatHost = h;
-    var p = d.location.protocol == "https:" ? "https://" : "http://";
-    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
-    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-})(window, document, 'script', 'cloud.roistat.com', '3c2ea0d78752c69ddccd41fcfa213168');
-</script>
-
+        
 	</head>
 <body>
     <!-- Google Tag Manager (noscript) -->
